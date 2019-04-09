b1469d0c74d29c1fc4c593c5606d8f45baf2afff
Soo Philip Jason Kim <philipjkim@gmail.com>
2019-04-09 21:02:55 +0900

    Get stdin for creating a new scoring rule

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+use std::error::Error;
+
+mod scoring_rule;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // create an app
+    let app = App::new("mlbh2h")
+        .version("0.1.0")
+        .author("Soo Philip Jason Kim <philipjkim@gmail.com>")
+        .about(
+            "This app Shows Yahoo! Baseball Head-to-Head fantasy points by your scoring settings.",
+        )
+        .subcommand(
+            SubCommand::with_name("add-rule")
+                .about("adds a fantasy point scoring rule")
+                .arg(
+                    Arg::with_name("name")
+                        .short("n")
+                        .help("set name of the new scoring rule")
+                        .takes_value(true)
+                        .required(true),
+                ),
+        );
+
+    if let Some(m) = app.get_matches().subcommand_matches("add-rule") {
+        let rule = scoring_rule::add_new_scoring_rule(m.value_of("name").unwrap());
+        match rule {
+            Ok(r) => {
+                println!("Saved the rule: {:#?}", r);
+                return Ok(());
+            }
+            Err(e) => return Err(e),
+        }
+    }
+    Ok(())
 }
