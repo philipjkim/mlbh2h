c25ca32ac36aead88f2906107e0623625a791978
Soo Philip Jason Kim <philipjkim@gmail.com>
2019-04-12 11:11:52 +0900

    Enhance parsing Cargo version

--- src/version.rs
+++ src/version.rs
@@ -1,15 +1,11 @@
-use std::fs;
+const TOML: &'static str = include_str!("../Cargo.toml");
 
 pub fn get() -> &'static str {
-    let text = fs::read_to_string("Cargo.toml").unwrap();
-    for line in text.split("\n") {
-        if line.starts_with("version") {
-            let start = line.find('"').unwrap() + 1;
-            let end = line.rfind('"').unwrap();
-            let version = &line[start..end];
-            return Box::leak(version.to_string().into_boxed_str());
-        }
+    if let Some(line) = TOML.split("\n").find(|x| x.starts_with("version")) {
+        let start = line.find('"').unwrap() + 1;
+        let end = line.rfind('"').unwrap();
+        &line[start..end]
+    } else {
+        panic!("failed parsing version from Cagro.toml");
     }
-
-    panic!("failed parsing version from Cagro.toml");
 }
