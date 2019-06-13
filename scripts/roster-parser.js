70428cc2fc25611f411bcb76a047275442d6be54
Soo Philip Jason Kim <philipjkim@gmail.com>
2019-06-13 14:28:00 +0900

    Update roster parser

--- scripts/roster-parser.js
+++ scripts/roster-parser.js
@@ -7,16 +7,21 @@ const $ = cheerio.load(contents)
 const teams = $('div.Bd').find('div.Grid-u-1-2.Pend-xl')
 let allPlayers = []
 teams.each((i, elem) => {
-    const team = $(elem).find('p a').first().text()
-    const players = $(elem).find('tbody tr').map((i, el) => {
-        const position = $(el).find('td.pos').first().text()
-        const role = ['SP', 'RP', 'P'].includes(position) ? 'Pitcher' : 'Batter'
-        const name = $(el).find('td.player div.Grid-bind-end div.ysf-player-name a.name').first().text()
+  const team = $(elem).find('p a').first().text()
+  const players = $(elem).find('tbody tr').map((i, el) => {
+    const position = $(el).find('td.pos').first().text()
+    let role
+    if (['BN', 'DL'].includes(position)) {
+      role = ''
+    } else {
+      role = ['SP', 'RP', 'P'].includes(position) ? 'Pitcher' : 'Batter'
+    }
+    const name = $(el).find('td.player div.Grid-bind-end div.ysf-player-name a.name').first().text()
 
-        return { name, role, team }
-    }).get()
+    return { name, role, team }
+  }).get()
 
-    allPlayers = allPlayers.concat(players)
+  allPlayers = allPlayers.concat(players.filter((p) => { return p.name !== '' && p.role !== '' }))
 })
 
 console.log(JSON.stringify({ players: allPlayers }, null, 2))
